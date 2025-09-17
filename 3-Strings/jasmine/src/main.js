c5deac6e00c70c89ad370444f36566fd7fa08ca9
BJUDON1 <Braylenjudson24@gmail.com>
2025-09-17 13:55:08 -0500

    submit

---
+++ 3-Strings/jasmine/src/main.js
@@ -0,0 +1,11 @@
+let line = "Stands so high" 
+let frontDoorLetter = line[0]
+let frontDoorWord = 'shire'
+let frontDoorPassword = frontDoorWord.charAt(0).toUpperCase() + frontDoorWord.slice(1,5)
+
+ let lineWithWhitespace = "Reins and harness"
+ let removeWhtespace = lineWithWhitespace.trim()
+let backDoorLetter = removeWhtespace[removeWhtespace.length - 1]
+let backDoorWord = "Horse";
+let backDoorPassword = backDoorWord + ", please"
+
