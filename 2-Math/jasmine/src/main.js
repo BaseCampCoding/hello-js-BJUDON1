de42061809ce262f4174d362dd9d5653ecd46592
BJUDON1 <Braylenjudson24@gmail.com>
2025-09-17 11:24:18 -0500

    submit2

--- 2-Math/jasmine/src/main.js
+++ 2-Math/jasmine/src/main.js
@@ -1,2 +1,17 @@
+let ratePerHour = 60;
+let dayRate = ratePerHour * 8;
+let budget = 1000;
+let workdays = Math.floor(budget/dayRate);
+console.log(workdays);
+let billable = 22;
+let monthlyDiscount = 0.8;
+let remainingDays = 35 - billable;
+let fullMonths = Math.floor(35/billable);
 
+
+
+console.log(fullMonths)
+let discountedMonthlyRate = (billable * dayRate * monthlyDiscount);
+let remainingCost = remainingDays * dayRate
+let totalProjectCost = Math.floor(discountedMonthlyRate + remainingCost );
   
