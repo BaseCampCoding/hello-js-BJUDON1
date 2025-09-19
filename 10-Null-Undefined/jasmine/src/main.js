f4d9a51f6775aa70487574cb510f14bad5bf021b
BJUDON1 <Braylenjudson24@gmail.com>
2025-09-19 13:12:45 -0500

    submit

---
+++ 10-Null-Undefined/jasmine/src/main.js
@@ -0,0 +1,22 @@
+let visitorName = 'Braylen'
+let visitorAge = 18
+let visitorTicketId = 'B-1234'
+let visitor ={
+    name: "Braylen",
+    age: 18,
+    ticketId: "B-1234"
+
+
+    
+}
+console.log(visitor)
+
+//task 2
+visitor.ticketId = null;
+console.log(visitor)
+
+//task 3 
+let tickts = {
+    'B-1234': null
+}
+ let ticketIdToCheck = tickts
